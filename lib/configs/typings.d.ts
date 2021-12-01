7dab1d923ebd8ea60e6c20feb4f2cee19b99df4e
xion <xiongxiao1012@outlook.com>
2021-12-01 23:07:28 +0800

    更新版本

---
+++ lib/configs/typings.d.ts
@@ -0,0 +1,16 @@
+declare module 'koa2-cors';
+declare module 'koa-onerror';
+
+// 序列化
+declare module 'mongoose-serial';
+
+declare module 'cors-anywhere';
+
+// mockjs http://mockjs.com/
+declare module 'mockjs';
+
+// type CTX: ParameterizedContext<any, DefaultContext & Router.IRouterParamContext<any, DefaultContext>>
+
+// declare module 'jsonwebtoken';
+
+declare var global: NodeJS.Global & typeof globalThis;
